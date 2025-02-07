7880c8c2857915d97a16d76d3d5c033574fe74b9
Egwaoje Daniel <egwaoje.daniel@gmail.com>
2025-02-07 05:32:48 +0100

    modals

--- my-react-app/src/component/employee.jsx
+++ my-react-app/src/component/employee.jsx
@@ -1,15 +1,16 @@
+import EmployeeEdit from "./employeeEdit";
+
 function Employee(props){
     return (
-        <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
-            <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="image" />
-                <div class="space-y-2 text-center sm:text-left">
-    <div class="space-y-0.5">
-      <p class="text-lg font-semibold text-black">{props.name}</p>
-      <p class="font-medium text-gray-500">{props.role}</p>
+        <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+            <img className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="image" />
+                <div className="space-y-2 text-center sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg font-semibold text-black">{props.name}</p>
+      <p className="font-medium text-gray-500">{props.role}</p>
     </div>
-    <button class="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
-      edit
-    </button>
+    <EmployeeEdit id={props.id} name={props.name} role={props.role} updateEmployee={props.updateEmployee}/>
+    
   </div>
 </div>
     )
